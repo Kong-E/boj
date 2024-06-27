093c890ef7cecdf9ed3a36c9a4738d902b5a3625
Soyeon Kong <87259219+Kong-E@users.noreply.github.com>
2024-06-27 16:24:46 +0900

    [Silver I] Title: 오목, Time: 144 ms, Memory: 14576 KB -BaekjoonHub

---
+++ Java/백준/Silver/2615. 오목/오목.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] dx = {1, 1, 1, 0};
+    static int[] dy = {0, 1, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int[][] board = new int[19][19];
+
+        for (int i = 0; i < 19; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 19; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        boolean found = false;
+        for (int y = 0; y < 19; y++) {
+            for (int x = 0; x < 19; x++) {
+                int current = board[y][x];
+                if (current != 0) {
+                    for (int i = 0; i < 4; i++) { // 4가지 방향
+                        int nx = x;
+                        int ny = y;
+                        int count = 1;
+                        while (true) {
+                            nx += dx[i];
+                            ny += dy[i];
+
+                            if (nx < 0 || nx >= 19 || ny < 0 || ny >= 19 || board[ny][nx] != current) {
+                                break;
+                            }
+
+                            count++;
+
+                            if (count == 5) {
+                                int isBackSixX = nx + dx[i];
+                                int isBackSixY = ny + dy[i];
+                                if (isBackSixY >= 0 && isBackSixY < 19 && isBackSixX >= 0 && isBackSixX < 19 && board[isBackSixY][isBackSixX] == current) {
+                                    break;
+                                }
+                                int isFrontSixX = x - dx[i];
+                                int isFrontSixY = y - dy[i];
+                                if (isFrontSixX >= 0 && isFrontSixX < 19 && isFrontSixY >= 0 && isFrontSixY < 19 && board[isFrontSixY][isFrontSixX] == current) {
+                                    break;
+                                }
+//                                System.out.println("현재방향: " + "(" + dx[i] + ", " + dy[i] + ")");
+//                                System.out.println("원래위치: " + "(" + (originX+1) + ", " + (originY+1) + ")");
+                                System.out.println(current);
+                                System.out.print((y + 1) + " ");
+                                System.out.print((x + 1));
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (found) break;;
+                    }
+                }
+                if (found) break;;
+            }
+            if (found) break;
+        }
+
+        if (!found) System.out.println(0);
+    }
+}
