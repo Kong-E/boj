7112e016edc3b2fa991e0e1f2153bcc1fea11d18
Soyeon Kong <87259219+Kong-E@users.noreply.github.com>
2024-06-30 12:55:09 +0900

    [Silver IV] Title: 설탕 배달, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

---
+++ Java/백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(count(n));
+    }
+
+    static int count(int n) {
+        for (int i = 0; i < 1700; i++) {
+            for (int j = 0; j < 1001; j++) {
+                int sum = 3*i + 5*j;
+                if (sum == n) {
+                    return i+j;
+                }
+            }
+        }
+        return -1;
+    }
+}
