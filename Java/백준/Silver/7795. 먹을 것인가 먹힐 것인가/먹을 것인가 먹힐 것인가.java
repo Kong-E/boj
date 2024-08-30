0b91f1f401d64fce79756ba62f6e4eaa652cdbe6
Soyeon Kong <87259219+Kong-E@users.noreply.github.com>
2024-08-30 23:29:00 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 940 ms, Memory: 42364 KB -BaekjoonHub

---
+++ Java/백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            int[] A = new int[n];
+            int[] B = new int[m];
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                A[i] = Integer.parseInt(st.nextToken());
+            }
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < m; i++) {
+                B[i] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(A);
+            Arrays.sort(B);
+
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (A[i] > B[j]) {
+                        count++;
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
+}
