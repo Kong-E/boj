716d2c912a8f8480f07c066b8a72b9931ff852c5
Soyeon Kong <87259219+Kong-E@users.noreply.github.com>
2024-08-31 22:53:52 +0900

    [level 1] Title: 덧칠하기, Time: 2.76 ms, Memory: 85.9 MB -BaekjoonHub

--- Java/프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ Java/프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -5,25 +5,27 @@ class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
         
-        boolean[] checked = new boolean[section.length];
-        
-        int left = section[0];
-        int right = left + m - 1;
-        int i = 0;
+        int left = section[0]; // 윈도우 시작점
+        int right = left + m - 1; // 윈도우 끝점
+        int i = 0; // section의 인덱스
         while (i < section.length) {
             int current = section[i];
-            if (current <= right && current >= left) {
-                checked[i] = true;
+            
+            // current가 윈도우 범위 내에 속하면
+            if (current <= right) {
                 i++;
             } else {
                 answer++;
-                left = current;
+                
+                left = current; // 속하지 않는 점을 윈도우의 시작점으로 지정
                 right = left + m - 1;
+                
+                // 윈도우의 끝점이 영역을 넘어가면 조정
                 if (right > n) {
-                    while (right > n) {
-                        left--;
-                        right = left + m - 1;
-                    }
+                    int diff = right - n;
+
+                    left -= diff;
+                    right = left + m - 1;
                 }
             }
         }
