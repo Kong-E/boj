713410c3ae386ff839b6941b9146c7595991e8e1
Soyeon Kong <87259219+Kong-E@users.noreply.github.com>
2024-09-28 16:17:01 +0900

    [Silver I] Title: 기타리스트, Time: 68 ms, Memory: 11812 KB -BaekjoonHub

---
+++ Java/백준/Silver/1495. 기타리스트/기타리스트.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        boolean[][] dp = new boolean[n + 1][1001];
+
+        dp[0][s] = true;
+        int max = -1;
+        st = new StringTokenizer(br.readLine());
+        int num;
+        for (int i = 1; i <= n; i++) {
+            num = Integer.parseInt(st.nextToken());
+            for (int j = 0; j <= 1000; j++) {
+                if (dp[i - 1][j]) {
+                    if (j + num <= m) {
+                        dp[i][j + num] = true;
+                        if (i == n) max = Math.max(max, j + num);
+                    }
+                    if (j - num >= 0) {
+                        dp[i][j - num] = true;
+                        if (i == n) max = Math.max(max, j - num);
+                    }
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
