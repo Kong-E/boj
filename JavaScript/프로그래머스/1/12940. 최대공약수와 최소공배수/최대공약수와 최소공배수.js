a978569b19acd63841403890a1d97d542a729d28
Soyeon Kong <87259219+Kong-E@users.noreply.github.com>
2024-11-08 20:04:11 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,13 @@
+function solution(n, m) {
+    
+    // 최대공약수
+    let max = 0;
+    for (let i = 1; i <= m; i++) {
+        if (n % i === 0 && m % i === 0) max = Math.max(max, i);
+    }
+    
+    // 최소공배수
+    let min = (n * m) / max;
+    
+    return [max, min];
+}
