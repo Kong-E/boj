77a3fdc78148bedabc2a687c8d12e18cf49b31fb
Soyeon Kong <87259219+Kong-E@users.noreply.github.com>
2025-01-15 10:40:24 +0900

    [level 2] Title: 전화번호 목록, Time: 136.09 ms, Memory: 81.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ JavaScript/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,34 +1,14 @@
 function solution(phone_book) {
-    let answer = true;
-    const map = new Map();
-    
-    // 정렬을 해야 접두어를 먼저 map에 넣을 수 있음
-    phone_book.sort((a, b) => a.length - b.length);
-    const minLength = phone_book[0].length;
-    
-    map.set(phone_book[0]);
-    
-    let cur;
+    // 기본 정렬: 사전순
+    phone_book.sort();
+
+    let prev, cur = phone_book[0];
     for (let i = 1; i < phone_book.length; i++) {
-        cur = phone_book[i];
-        for (let j = minLength; j < cur.length; j++) {
-            if (map.has(cur.slice(0, j))) {
-                return false;
-            }
+        [prev, cur] = [cur, phone_book[i]];
+        if (cur.startsWith(prev)) {
+            return false;
         }
-        map.set(cur);
     }
     
-    return answer;
+    return true;
 }
-
-// 접두어인 경우가 있으면 false;
-// 아니면 true
-
-// 1 <= phone_book 배열 길이 <= 100만
-// 1 <= 각 전화번호의 길이 <= 20
-
-// phone_book을 전화번호.길이를 기준으로 정렬
-// 각 전화번호마다 전화번호.길이만큼 for문 돌리기
-// 돌리면서 map.has()면 return false
-// 아니라면 map.set(cur);
