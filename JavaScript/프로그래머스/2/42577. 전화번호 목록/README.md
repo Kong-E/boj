c81f699738593b7e0926d731e869f7c072da4957
Soyeon Kong <87259219+Kong-E@users.noreply.github.com>
2025-01-15 09:57:28 +0900

    [level 2] Title: 전화번호 목록, Time: 46.52 ms, Memory: 80.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42577. 전화번호 목록/README.md
+++ JavaScript/프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=javascript) 
 
 ### 성능 요약
 
-메모리: 94.9 MB, 시간: 327.53 ms
+메모리: 80.1 MB, 시간: 46.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 15일 09:43:27
+2025년 01월 15일 09:57:26
 
 ### 문제 설명
 
--- JavaScript/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ JavaScript/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -2,19 +2,21 @@ function solution(phone_book) {
     let answer = true;
     const map = new Map();
     
+    // 정렬을 해야 접두어를 먼저 map에 넣을 수 있음
     phone_book.sort((a, b) => a.length - b.length);
+    const minLength = phone_book[0].length;
     
     map.set(phone_book[0]);
     
+    let cur;
     for (let i = 1; i < phone_book.length; i++) {
-        const cur = phone_book[i];
-        for (let j = 1; j < cur.length; j++) {
+        cur = phone_book[i];
+        for (let j = minLength; j < cur.length; j++) {
             if (map.has(cur.slice(0, j))) {
                 return false;
-            } else {
-                map.set(cur);
             }
         }
+        map.set(cur);
     }
     
     return answer;
@@ -27,7 +29,6 @@ function solution(phone_book) {
 // 1 <= 각 전화번호의 길이 <= 20
 
 // phone_book을 전화번호.길이를 기준으로 정렬
-// let last_length = phone_book[0].length
 // 각 전화번호마다 전화번호.길이만큼 for문 돌리기
-// 돌리면서 map.containsKey()면 return false
-// 아니라면 map.set(cur); last_length = cur.length;
+// 돌리면서 map.has()면 return false
+// 아니라면 map.set(cur);
