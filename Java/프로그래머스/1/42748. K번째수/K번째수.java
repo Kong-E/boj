b4e2d2c19423dee6a5093edb2243c349179246d0
Soyeon Kong <87259219+Kong-E@users.noreply.github.com>
2025-01-20 07:46:02 +0900

    [level 1] Title: K번째수, Time: 0.39 ms, Memory: 88.7 MB -BaekjoonHub

--- Java/프로그래머스/1/42748. K번째수/K번째수.java
+++ Java/프로그래머스/1/42748. K번째수/K번째수.java
@@ -5,8 +5,9 @@ class Solution {
         int len = commands.length;
         int[] answer = new int[len];
         
+        int[] subArray;
         for (int i = 0; i < len; i++) {
-            int[] subArray = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            subArray = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
             Arrays.sort(subArray);
             answer[i] = subArray[commands[i][2] - 1];
         }
