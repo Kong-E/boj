991fac0ff8f19d49c83ad3c8f2725493e1d7825f
Soyeon Kong <87259219+Kong-E@users.noreply.github.com>
2025-02-06 23:39:41 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 3774.91 ms, Memory: 722 MB -BaekjoonHub

--- Java/프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ Java/프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,86 +1,96 @@
 import java.util.*;
 
 class Solution {
-    static class Position {
-        int r, c, time;
-        
-        Position(int r, int c, int time) {
+    class Robot {
+        int r;
+        int c;
+        int num; // 몇 번째 로봇인지
+        int nextIdx; // 다음 경로의 인덱스
+        int count; // 몇 번 움직였는지
+
+        Robot(int r, int c, int num, int nextIdx, int count) {
             this.r = r;
             this.c = c;
-            this.time = time;
-        }
-        
-        @Override
-        public String toString() {
-            return time + "(" + r + "," + c + ")";
+            this.num = num;
+            this.nextIdx = nextIdx;
+            this.count = count;
         }
     }
-    
+
     public int solution(int[][] points, int[][] routes) {
-        // 포인트 번호 -> 좌표 매핑
+        int[][] board = new int[101][101];
+
+        // 포인트 번호 - 좌표 매핑
         Map<Integer, int[]> pointMap = new HashMap<>();
         for (int i = 0; i < points.length; i++) {
             pointMap.put(i + 1, points[i]);
         }
-        
-        // 시간별 위치에 있는 로봇 수를 기록
-        Map<String, Integer> collisionMap = new HashMap<>();
-        
-        // 각 로봇의 경로를 시뮬레이션
-        for (int robotId = 0; robotId < routes.length; robotId++) {
-            List<Position> path = new ArrayList<>();
-            
-            // 각 경로 구간별로 이동 경로 계산
-            for (int i = 0; i < routes[robotId].length - 1; i++) {
-                int[] start = pointMap.get(routes[robotId][i]);
-                int[] end = pointMap.get(routes[robotId][i + 1]);
-                
-                int startTime = path.isEmpty() ? 0 : path.get(path.size() - 1).time + 1;
-                List<Position> segment = getPath(start[0], start[1], end[0], end[1], startTime);
-                path.addAll(segment);
+
+        // 각 좌표 별로 방문 카운팅: (몇번째로 방문 + 좌표) = key
+        Map<String, Integer> visitMap = new HashMap<>();
+
+        Queue<Robot> q = new LinkedList<>();
+
+        // 각 로봇의 (첫번째 경로의 좌표 + 몇번째 로봇인지)를 큐에 넣어놓기
+        for (int i = 0; i < routes.length; i++) {
+            int[] route = routes[i];
+            int[] startPoint = pointMap.get(route[0]);
+            q.offer(new Robot(startPoint[0], startPoint[1], i + 1, 1, 1));
+            visitMap.put(getKey(1, startPoint[0], startPoint[1]), visitMap.getOrDefault(getKey(1, startPoint[0], startPoint[1]), 0) + 1);
+        }
+
+        while (!q.isEmpty()) {
+            // 큐에서 꺼내기
+            Robot robot = q.poll();
+            int curR = robot.r, curC = robot.c, count = robot.count;
+            int[] nextPoint = pointMap.get(routes[robot.num - 1][robot.nextIdx]);
+            int nextR = nextPoint[0], nextC = nextPoint[1];
+
+            // 다음 경로까지의 최단 경로를 구하기
+            // 최단 경로가 여러 개면
+            if (curR != nextR) {
+                if (curR > nextR) {
+                    curR--;
+                } else {
+                    curR++;
+                }
+                count++;
+            } else if (curC != nextC) {
+                if (curC > nextC) {
+                    curC--;
+                } else {
+                    curC++;
+                }
+                count++;
             }
             
-            // 마지막 위치 추가
-            if (!path.isEmpty()) {
-                int[] lastPoint = pointMap.get(routes[robotId][routes[robotId].length - 1]);
-                path.add(new Position(lastPoint[0], lastPoint[1], path.get(path.size() - 1).time + 1));
+            // 방문 카운팅하기
+            visitMap.put(getKey(count, curR, curC), visitMap.getOrDefault(getKey(count, curR, curC), 0) + 1);
+
+            // 만약 다음 경로와 일치하는 좌표라면 다음 경로 인덱스 업데이트(Robot.next 업데이트)
+            if (curR == nextR && curC == nextC) {
+                // 다음 경로가 있는지 확인
+                if (routes[robot.num - 1].length - 1 > robot.nextIdx) {
+                    robot.nextIdx++;
+                } else {
+                    continue;
+                }
             }
-            
-            // 경로의 각 위치를 collision map에 기록
-            for (Position pos : path) {
-                String key = pos.toString();
-                collisionMap.put(key, collisionMap.getOrDefault(key, 0) + 1);
-            }
+
+            q.offer(new Robot(curR, curC, robot.num, robot.nextIdx, count));
         }
         
-        // 충돌 횟수 계산
         int answer = 0;
-        for (int count : collisionMap.values()) {
-            if (count > 1) answer++;
+        for (String key : visitMap.keySet()) {
+            if (visitMap.get(key) > 1) {
+                answer++;
+            }
         }
         
         return answer;
     }
     
-    // 두 점 사이의 경로를 반환하는 메서드
-    private List<Position> getPath(int startR, int startC, int endR, int endC, int startTime) {
-        List<Position> path = new ArrayList<>();
-        int currentR = startR;
-        int currentC = startC;
-        int time = startTime;
-        
-        // r 좌표 이동
-        while (currentR != endR) {
-            path.add(new Position(currentR, currentC, time++));
-            currentR += (endR > currentR) ? 1 : -1;
-        }
-        
-        // c 좌표 이동
-        while (currentC != endC) {
-            path.add(new Position(currentR, currentC, time++));
-            currentC += (endC > currentC) ? 1 : -1;
-        }
-        
-        return path;
+    public String getKey(int count, int r, int c) {
+        return String.format("%d(%d,%d)", count, r, c);
     }
 }
