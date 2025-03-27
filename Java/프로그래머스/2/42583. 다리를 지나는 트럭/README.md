a2719a10dcc99d12fe5fd5f9f17d23b1433c0aed
Soyeon Kong <87259219+Kong-E@users.noreply.github.com>
2025-03-27 19:21:52 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 24.57 ms, Memory: 78.5 MB -BaekjoonHub

--- Java/프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ Java/프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 95.1 MB, 시간: 38.33 ms
+메모리: 78.5 MB, 시간: 24.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 27일 19:07:44
+2025년 03월 27일 19:21:46
 
 ### 문제 설명
 
--- Java/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ Java/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -3,32 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
 
-        // truck_weights에서 for문 돌리며, 다리에 올라갈 수 있는지 확인
-        // 1. 올라갈 수 없으면 
-        // -> 큐에 있는 것들이 다 건너가기를 기다리기, 카운트
-        // 2. 올라갈 수 있으면
-        // -> 큐에 추가
         Queue<Integer> q = new LinkedList<>();
         
         int totalW = 0;
         int second = 0;
         int idx = 0;
         do {
-            if (idx == truck_weights.length) break;
             if (q.size() == bridge_length) {
                 totalW -= q.poll();
             }
-            int w = truck_weights[idx];
-            if (totalW + w > weight) {
+            if (idx == truck_weights.length || totalW + truck_weights[idx] > weight) {
                 q.offer(0);
             } else {
-                q.offer(w);
-                totalW += w;
+                q.offer(truck_weights[idx]);
+                totalW += truck_weights[idx];
                 idx++;
             }
             second += 1;
-        } while (!q.isEmpty());
+        } while (totalW != 0);
 
-        return second + bridge_length;
+        return second;
     }
 }
