f1d5fdedc282b1f856bfef332095dd449a4417a4
Soyeon Kong <87259219+Kong-E@users.noreply.github.com>
2025-03-28 19:34:02 +0900

    [level 2] Title: 주식가격, Time: 36.46 ms, Memory: 72.1 MB -BaekjoonHub

--- Java/프로그래머스/2/42584. 주식가격/주식가격.java
+++ Java/프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,14 +1,26 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        for (int i = 0; i < prices.length; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
-                answer[i]++;
-                if (prices[i] > prices[j]) {
-                    break;
-                }
+        int n = prices.length;
+        int[] answer = new int[n];
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                // 가격이 떨어진 것
+                int idx = stack.pop();
+                answer[idx] = i - idx;
             }
+            stack.push(i);
         }
+        
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = n - 1 - idx;
+        }
+        
         return answer;
     }
 }
